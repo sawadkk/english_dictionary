b362e37c4a50e78ac2a229c8d67a6e013d4fd47c
sawadkk <ssawad09@gmail.com>
2021-07-10 19:09:49 +0530

    acronyms

--- core.py
+++ core.py
@@ -9,6 +9,8 @@ def translate(word):
 		return data[word]
 	elif word.title() in data:
 		return data[word.title()]
+	elif word.upper() in data:
+		return data[word.upper()]
 	elif len(get_close_matches(word, data.keys())) > 0:
 		yn = input("Did you mean %s instead? Enter Y if yes ,or N for no: " % get_close_matches(word,data.keys())[0])
 		if yn == 'Y' or yn == 'y':
